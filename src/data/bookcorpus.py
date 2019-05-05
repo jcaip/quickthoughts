2060e47900c71f001593e548c5dafba78980b00d
Jesse Cai <jcjessecai@gmail.com>
2019-05-05 14:04:10 -0700

    made some more progress

--- src/data/bookcorpus.py
+++ src/data/bookcorpus.py
@@ -1,29 +1,103 @@
 import torch
-import torch.nn 
+import torch.nn  as nn
 import nltk
 from gensim.utils import tokenize
-from torchtext.data.dataset import Dataset
-import torchtext.data as data
+import torch.optim as optim
+import torch.utils.data as data
+import numpy as np
+from gensim.models.keyedvectors import KeyedVectors
 
-data_path = '/home/jcaip/workspace/quickthoughts/bookcorpus/head.txt'
+data_path = '/home/jcaip/workspace/quickthoughts/bookcorpus/all.txt'
+vec_path = '/home/jcaip/workspace/quickthoughts/S2V/GoogleNews-vectors-negative300.bin.gz'
 
-def tokenizer(text):
-    return [tok for tok in tokenize(text)]
+def prepare_sequence(text, vocab):
+    return torch.LongTensor(map(lambda x: vocab[x].index, filter(lambda w: w in vocab, tokenize(text))))
 
-TEXT = data.Field(sequential=True, tokenize=tokenizer)
-fields=[('Text', TEXT)]
+class BookCorpus(data.dataset.Dataset):
 
-print("Reading the data")
-with open(data_path, encoding='ISO-8859-1') as f:
-    bc = Dataset([data.Example.fromlist([x.strip()], fields) for x in f if x.strip() != ''], fields=fields)
+    def __init__(self, file_path=data_path):
+        print("Reading the data")
+        with open(data_path, encoding='ISO-8859-1') as f:
+            print("Creating Dataset")
+            examples_list = map(lambda x: prepare_sequence(text, vocab), fields), f)
 
-TEXT.build_vocab(bc, vectors="glove.6B.100d")
-train_iter = data.Iterator(bc, batch_size=64)
+    def __getitem__(i):
 
-for i in train_iter:
-    print(i)
-    print(type(i))
-    print(i.Text)
-    # vocab = TEXT.vocab
-    # self.embed = nn.Embedding(len(vocab), emb_dim)
-    # self.embed.weight.data.copy_(vocab.vectors)
+    print("Building Vocab!")
+
+    def 
+
+
+class BOWEncoder(nn.Module):
+
+    def __init__(self, embedding_filepath=vec_path, vocab=None):
+        super(BOWEncoder, self).__init__()
+
+        self.wv_model = KeyedVectors.load_word2vec_format(embedding_filepath, binary=True, limit=vocab)
+        self.embeddings = nn.Embedding(*self.wv_model.vectors.shape)
+        self.embeddings.weight.data.copy_(vocab.vectors)
+
+    def forward(self, inputs):
+        embeds = self.embeddings(inputs)
+        # print(embeds.shape)
+        max, argmax = torch.max(embeds, dim=0)
+        return max
+
+
+class QuickThoughts(nn.Module):
+
+    def __init__(self,vocab,  encoder='bow'):
+        super(QuickThoughts, self).__init__()
+
+        if encoder =='bow':
+            self.enc_f = BOWEncoder(vocab)
+            self.enc_g = BOWEncoder(vocab)
+
+    def forward(self, inputs):
+        encoding_f = self.enc_f(inputs)
+        encoding_g = self.enc_g(inputs)
+
+        # print("Encoding_f shape: {}".format(encoding_f.shape))
+        # print("Encoding_g shape: {}".format(encoding_g.shape))
+
+        scores = torch.matmul(encoding_f, encoding_g.t())
+
+        # print("scores shape: {}".format(scores.shape))
+        mask = torch.eye(len(scores)).byte()
+        scores.masked_fill_(mask, 0)    
+
+        return scores
+
+
+    batch_size = 64
+    context_size = 1
+    train_iter = data.Iterator(bc, batch_size=batch_size)
+
+    qt = QuickThoughts(TEXT.vocab)
+    optimizer = optim.Adam(qt.parameters(), lr=5e-4)
+    loss_function = nn.BCEWithLogitsLoss()
+
+    print("Starting training")
+    for i, data in enumerate(train_iter):
+        # print(i.text.shape)
+        a = qt(data.text)
+        # print(a)
+        # print(a.shape)
+
+        # batch_size = min(batch_size, len(i.text))
+        targets_np = np.zeros((batch_size, batch_size))
+        ctxt_sent_pos = list(range(-context_size, context_size + 1))
+        ctxt_sent_pos.remove(0)
+        for ctxt_pos in ctxt_sent_pos:
+            targets_np += np.eye(batch_size, k=ctxt_pos)
+
+        targets_np_sum = np.sum(targets_np, axis=1, keepdims=True)
+        targets_np = targets_np/targets_np_sum
+        targets = torch.tensor(targets_np, dtype=torch.float)
+        # print(targets)
+        # print(targets.shape)
+
+        loss = loss_function(a, targets)
+        print("step {} loss: {}".format(i, loss))
+        loss.backward()
+        optimizer.step()
--- src/util.py
+++ src/util.py
@@ -8,8 +8,6 @@ import operator
 import base64
 import json
 import logging
-import numpy as np
-import pandas as pd
 
 logging.basicConfig(
     format='%(asctime)s %(levelname)-8s %(message)s',
@@ -19,42 +17,3 @@ logging.basicConfig(
 _LOGGER = logging.getLogger(__name__)
 
 
-def load_data(labeled_files, unlabeled_files):
-    """load_data
-
-    :param labeled_file:
-    :param unlabeled_filename:
-    :param num_unfrozen:
-    """
-    # loading nps comment dataframe
-    df_labeled = pd.concat(map(pd.read_excel, labeled_files))
-    df_unlabeled = pd.concat(map(pd.read_excel, unlabeled_files))
-
-    # we shouldn't need to do this but we do :(
-    df_labeled = df_labeled[df_labeled['nps_comments'].notnull()]
-    df_unlabeled = df_unlabeled[df_unlabeled['nps_comments'].notnull()]
-
-    # filter out the labeled comments
-    df_unlabeled = df_unlabeled[df_unlabeled.id.apply(lambda x: x not in df_labeled.id)]
-
-    _LOGGER.info("loaded {} labeled comments | {} unlabeled comments".format(len(df_labeled),
-                                                                             len(df_unlabeled)))
-    return (df_labeled, df_unlabeled)
-
-
-# here tokenizing is simple, but we get rid of very common words
-def tokenize_vocab(text, vocab):
-    """tokenize_vocab
-
-    :param text:
-    :param vocab:
-    """
-    return list(filter(lambda w: w in vocab, tokenize(text.lower())))
-
-# map to y
-def categorize(label):
-    return 1 if label == 'y' else 0
-
-# formats sequence of words into a tensor
-def prepare_sequence(seq, vocab):
-    return torch.LongTensor([vocab[w].index for w in seq])
