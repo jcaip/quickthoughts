ec907f2a5fb174ad2984c0db2e970115785ccc99
Jesse Cai <jcjessecai@gmail.com>
2019-05-05 20:03:24 -0700

    first trial run done

--- src/data/bookcorpus.py
+++ src/data/bookcorpus.py
@@ -7,6 +7,7 @@ import torch.utils.data as data
 import numpy as np
 from gensim.models.keyedvectors import KeyedVectors
 from torch.nn.utils.rnn import pack_padded_sequence, pad_packed_sequence, pad_sequence
+from pprint import pprint
 
 from visdom import Visdom
 
@@ -56,12 +57,11 @@ class BookCorpus(data.dataset.Dataset):
 
 class Encoder(nn.Module):
 
-    def __init__(self, embedding_filepath=vec_path, vocab=None, hidden_dim=2400):
+    def __init__(self, embedding_filepath=vec_path, vocab=None, hidden_dim=1000):
         super(Encoder, self).__init__()
         self.hidden_size = hidden_dim
 
         self.embeddings = nn.Embedding(*wv_model.vectors.shape)
-        print(wv_model.vectors.shape)
         self.embeddings.weight = torch.nn.Parameter(torch.from_numpy(wv_model.vectors), 
                                                     requires_grad=False)
         self.gru = nn.GRU(wv_model.vectors.shape[1], hidden_dim)
@@ -72,18 +72,19 @@ class Encoder(nn.Module):
         if True:
             hidden = hidden.cuda()
         out, hidden = self.gru(embeds, hidden)
-        return hidden[-1]
 
+        return out[-1]
 
 class QuickThoughts(nn.Module):
 
     def __init__(self, encoder='bow'):
         super(QuickThoughts, self).__init__()
         self.enc_f = Encoder().cuda()
+        self.enc_g = Encoder().cuda()
 
     def forward(self, inputs):
         encoding_f = self.enc_f(inputs)
-        encoding_g = self.enc_f(inputs)
+        encoding_g = self.enc_g(inputs)
 
         # print("Encoding_f shape: {}".format(encoding_f.shape))
         # print("Encoding_g shape: {}".format(encoding_g.shape))
@@ -95,7 +96,6 @@ class QuickThoughts(nn.Module):
         scores.masked_fill_(mask, 0)    
 
         return scores
-
 
 bc = BookCorpus()
 batch_size = 400
@@ -111,7 +111,7 @@ for name, param in qt.named_parameters():
             print(name, param.data.size())
 
 optimizer = optim.Adam(filter(lambda p: p.requires_grad, qt.parameters()), lr=5e-4)
-loss_function = nn.BCEWithLogitsLoss()
+loss_function = nn.BCEWithLogitsLoss(reduction='sum')
 
 plotter = VisdomLinePlotter(env_name='qt')
 norm_threshold=1.0 
@@ -123,10 +123,9 @@ for i, data in enumerate(train_iter):
     qt.zero_grad()
     data = data.cuda()
     a = qt(data)
-    # print(a)
-    # print(a.shape)
 
-    # batch_size = min(batch_size, len(i.text))
+    # print(a.shape)
+    #TODO: Reformat this
     targets_np = np.zeros((batch_size, batch_size))
     ctxt_sent_pos = list(range(-context_size, context_size + 1))
     ctxt_sent_pos.remove(0)
@@ -136,14 +135,25 @@ for i, data in enumerate(train_iter):
     targets_np_sum = np.sum(targets_np, axis=1, keepdims=True)
     targets_np = targets_np/targets_np_sum
     targets = torch.tensor(targets_np, dtype=torch.float).cuda()
-    # print(targets)
-    # print(targets.shape)
 
     loss = loss_function(a, targets)
-    if i % 10 == 0:
+    plotter.plot('loss', 'train', 'Loss', i, loss.item())
+    if i % 50 == 0:
         print("step {} loss: {}".format(i, loss))
-        plotter.plot('loss', 'train', 'Class Loss', i, loss.item())
+
+        test_sentences =  [ "What is going on?",
+                           "Let's go eat.",
+                           "The engine won't start.",
+                           "I'm hungry now."]
+
+        pprint(test_sentences)
+
+        test_sentences = pad_sequence(list(map(prepare_sequence, test_sentences))).cuda()
+        print(qt(test_sentences))
+
+    if i % 1000 == 0: 
+        torch.save(qt.state_dict(), "../checkpoints/model-{}.pth".format(i))
 
     loss.backward()
-    nn.utils.clip_grad_norm(filter(lambda p: p.requires_grad, qt.parameters()), norm_threshold)
+    nn.utils.clip_grad_norm_(filter(lambda p: p.requires_grad, qt.parameters()), norm_threshold)
     optimizer.step()
