994657d860cce956ec7b7da9006bfc9a20412da9
Jesse Cai <jcjessecai@gmail.com>
2019-05-06 11:22:13 -0700

    modified

--- src/util.py
+++ src/util.py
@@ -1,6 +1,9 @@
 import logging
 from gensim.models import KeyedVectors
+from gensim.utils import tokenize
+from visdom import Visdom
 import numpy as np
+import torch
 
 logging.basicConfig(
     format='%(asctime)s %(levelname)-8s %(message)s',
@@ -9,27 +12,22 @@ logging.basicConfig(
     datefmt='%Y-%m-%d %H:%M:%S')
 
 _LOGGER = logging.getLogger(__name__)
-import torch
-
 
 vec_path = '/home/jcaip/workspace/quickthoughts/GoogleNews-vectors-negative300.bin'
 wv_model = KeyedVectors.load_word2vec_format(vec_path, binary=True, limit=10000)
-
-from gensim.utils import tokenize
 
 def prepare_sequence(text, vocab=wv_model.vocab):
     tokens = list(tokenize(text))
     pruned = tokens[:min(50, len(tokens))]
     return torch.LongTensor([vocab[x].index for x in filter(lambda w: w in vocab, pruned)])
 
-from visdom import Visdom
-
 class VisdomLinePlotter(object):
-    """Plots to Visdom"""
+
     def __init__(self, env_name='main'):
         self.viz = Visdom()
         self.env = env_name
         self.plots = {}
+
     def plot(self, var_name, split_name, title_name, x, y):
         if var_name not in self.plots:
             self.plots[var_name] = self.viz.line(X=np.array([x,x]), Y=np.array([y,y]), env=self.env, opts=dict(
