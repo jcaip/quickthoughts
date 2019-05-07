00cae20814d0d719a050f6418e099fb512813d91
Jesse Cai <jcjessecai@gmail.com>
2019-05-07 13:45:16 -0700

    test

--- src/bookcorpus.py
+++ src/bookcorpus.py
@@ -4,12 +4,18 @@ import multiprocessing
 import json
 import torch
 
+def prepare_sequence(text, vocab=_WV_MODEL.vocab, max_len=50, return_tensor=True):
+    pruned_sequence = zip(filter(lambda x: x in vocab, text), range(max_len))
+    seq = [vocab[x].index for x, _ in pruned_sequence]
+    return seq
+
+
 #this function  should  process all.txt and removes all lines that are empty assuming the vocab
 def preprocess(file_path, write_path, vocab=_WV_MODEL.vocab, max_len=50):
     pool = multiprocessing.Pool(10)
     with open(file_path, encoding='ISO-8859-1') as read_file, open(write_path, "w+") as write_file:
         i, j= 0, 0
-        for result in pool.imap(prepare_sequence, read_file, return_tensor=False):
+        for result in pool.imap(prepare_sequence, read_file):
             i+=1
             if len(result) != 0:
                j+=1
@@ -25,6 +31,7 @@ class BookCorpus(Dataset):
             self.examples = list(f)
 
     def __getitem__(self, i):
+        # this should be faster??
         return torch.LongTensor(json.loads(self.examples[i]))
 
     def __len__(self):
