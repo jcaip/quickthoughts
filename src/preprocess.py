8b9a353c29d82cd90e35a9833e74d9c8b4e6a3de
Jesse Cai <jcjessecai@gmail.com>
2019-05-14 16:04:24 -0700

    updated preprocess

--- src/preprocess.py
+++ src/preprocess.py
@@ -3,12 +3,13 @@ import time
 from config import CONFIG
 from data.utils import preprocess
 from gensim.models import KeyedVectors
+import gensim.downloader as api
 
 _LOGGER = logging.getLogger(__name__)
 _LOGGER.setLevel(logging.INFO)
 
 if __name__ == '__main__':
-    WV_MODEL = KeyedVectors.load_word2vec_format(CONFIG['vec_path'], binary=True, limit=CONFIG['vocab_size'])
+    WV_MODEL = api.load('glove-wiki-gigaword-300')
     _LOGGER.info("Starting data preprocessing")
     start = time.time()
     preprocess("{}/all.txt".format(CONFIG['base_dir']), "{}/cleaned.txt".format(CONFIG['base_dir']), WV_MODEL.vocab)
