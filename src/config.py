4fe4673a5551aa5b07ff92001502b818f5040bfd
Jesse Cai <jcjessecai@gmail.com>
2019-05-15 00:34:16 +0000

    cleaned up config

--- src/config.py
+++ src/config.py
@@ -2,19 +2,18 @@ import os
 import time 
 from datetime import datetime
 
-__base_dir = os.getenv('DIR', '~/quickthoughts')
+__base_dir = os.getenv('DIR', '/home/jcaip/quickthoughts')
 
 CONFIG = {
     'base_dir': __base_dir,
     'vec_path': '{}/data/GoogleNews-vectors-negative300.bin'.format(__base_dir),
-    'data_path': '{}/data/all.txt'.format(__base_dir),
+    'data_path': '{}/data/cleaned.txt'.format(__base_dir),
     'checkpoint_dir': '{}/checkpoints/{:%m-%d-%H-%M-%S}'.format(__base_dir, datetime.now()), 
     'resume': False,
     'context_size': 1,
     'batch_size': 400,
-    'norm_threshold': 1.0,
-    'hidden_size': 2400,
+    'norm_threshold': 5.0,
+    'hidden_size': 1000,
     'num_epochs': 5,
     'lr': 5e-4,
-    'vocab_size': 50000,
 }
--- src/data/utils.py
+++ src/data/utils.py
@@ -1,4 +1,5 @@
 from operator import itemgetter
+from tqdm import tqdm
 
 def prepare_sequence(text, vocab, max_len=50):
     pruned_sequence = zip(filter(lambda x: x in vocab, text), range(max_len))
@@ -9,6 +10,6 @@ def prepare_sequence(text, vocab, max_len=50):
 def preprocess(read_path, write_path, vocab, max_len=50):
     with open(read_path) as read_file, open(write_path, "w+") as write_file:
         # should all be iterators so fast
-        write_file.writelines(line for _ , line in filter(itemgetter(0),
+        write_file.writelines(line for _ , line in tqdm(filter(itemgetter(0),
                                                           map(lambda x: prepare_sequence(x, vocab, max_len=max_len),
-                                                              read_file)))
+                                                              read_file))))
